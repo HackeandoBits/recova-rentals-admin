d72c3fdfd580b4bd0c23bbb3e4fb48687f315391
JaJo EkiZ <jajoultrapunk@gmail.com>
2025-12-11 05:28:37 -0300

    fix: agrega los archivos de configracion ngnix.conf y startup.sh

---
+++ startup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# 1. Copiar configuración de Nginx (Sobreescribimos la default)
+cp /home/site/wwwroot/nginx.conf /etc/nginx/sites-available/default
+service nginx reload
+
+# 2. Caché de Laravel (Vital para producción)
+php /home/site/wwwroot/artisan config:cache
+php /home/site/wwwroot/artisan route:cache
+php /home/site/wwwroot/artisan view:cache
+
+# 3. Enlace simbólico para imágenes (Hotspots)
+php /home/site/wwwroot/artisan storage:link
